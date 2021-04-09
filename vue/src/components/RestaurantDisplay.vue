41f7fc262ec12219eebc19eada24f2a6ba076f7e
Robin Blake <robindeanblake@yahoo.com>
2021-04-09 09:57:42 -0400

    More styling

--- vue/src/components/RestaurantDisplay.vue
+++ vue/src/components/RestaurantDisplay.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>
+  <div id="main">
+    <img class="backgroundLogo" src="https://www.linkpicture.com/q/bg4.png"/><br>
     <div
       class="restaurant-list"
       v-for="restaurant in $store.state.returnedRestaurants"
@@ -15,16 +16,16 @@
           <img
             v-bind:src="restaurant.image_url"
             alt=""
-            width="250"
-            height="250"
+            width="150"
+            height="150"
           />
         </td>
-        <td>
+        <td class="display-text">
           {{ restaurant.name }}{{ restaurant.display_phone }}
         </td>
       </tr>
+      </div>
     </div>
-  </div>
 </template>
 
 <script>
@@ -43,4 +44,29 @@ export default {
 </script>
 
 <style>
+#main {
+display: grid;
+align-items: center;
+justify-content: center;
+padding-top: 30px;
+}
+    .backgroundLogo{
+      min-height: 100%;
+      min-width: 1024px;
+      width: 100%;
+      height: auto;
+      position: fixed;
+      top: 0;
+      left: 0;
+      z-index: -1;
+      background-size: cover;
+    }
+     .display-text {
+       vertical-align: middle;
+        font-family: 'Montserrat', sans-serif;
+        color: white;
+        padding-left: 20px;
+     }
+@import url('https://fonts.googleapis.com/css2?family=Anton&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Anton&family=Montserrat:wght@500&display=swap');
 </style>
