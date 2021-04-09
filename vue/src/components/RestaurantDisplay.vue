02aee3945ae44ba3efc674c757749c93ababdeb3
jwilsonwoodrow <77342292+jwilsonwoodrow@users.noreply.github.com>
2021-04-09 13:53:41 -0400

    RestaurantDetailsController created and linked, started work parsing details, created getRestaurantDetails method

--- vue/src/components/RestaurantDisplay.vue
+++ vue/src/components/RestaurantDisplay.vue
@@ -2,7 +2,7 @@
   <div>
     <div
       class="restaurant-list"
-      v-for="restaurant in $store.state.returnedRestaurants.businesses"
+      v-for="(restaurant, index) in $store.state.returnedRestaurants.businesses"
       v-bind:key="restaurant.id"
       v-show="$store.state.returnedRestaurants"
     >
@@ -20,10 +20,17 @@
           />
         </td>
         <td>
-          {{ restaurant.name }}<br>{{ restaurant.display_phone }}<br>{{ restaurant.location.address1}}<br>{{ restaurant.location.city}}, {{ restaurant.location.state}} {{ restaurant.location.zip_code}}<br>{{ restaurant.is_closed}}
+          {{ restaurant.name }}<br />{{ restaurant.display_phone }}<br />{{
+            restaurant.location.address1
+          }}<br />{{ restaurant.location.city }},
+          {{ restaurant.location.state }} {{ restaurant.location.zip_code
+          }}<br />{{ restaurant.is_closed }}
         </td>
         <td>
-          <button @click="getRestaurantHours(restaurant.id)"> </button>
+          <button @click="getRestaurantDetails(restaurant.id, index)"></button>
+          <div v-show="displayDetails[index]">
+            {{ rawDataDetails.hours }}
+          </div>
         </td>
       </tr>
     </div>
@@ -31,68 +38,76 @@
 </template>
 
 <script>
+import apiService from "../services/apiService";
 export default {
   name: "restaurant-display",
   components: {},
   data() {
     return {
+      displayDetails: [], //array of booleans, one for each displayed restaurant
+      rawDataDetails: [],
       hoursOfOperation: [
         {
           dayID: 6,
           dayOfWeek: "Sunday",
           openTime: 0,
-          closeTime: 0
+          closeTime: 0,
         },
         {
           dayID: 0,
           dayOfWeek: "Monday",
           openTime: 0,
-          closeTime: 0
+          closeTime: 0,
         },
         {
           dayID: 1,
           dayOfWeek: "Tuesday",
           openTime: 0,
-          closeTime: 0
+          closeTime: 0,
         },
         {
           dayID: 2,
           dayOfWeek: "Wednesday",
           openTime: 0,
-          closeTime: 0
+          closeTime: 0,
         },
         {
           dayID: 3,
           dayOfWeek: "Thursday",
           openTime: 0,
-          closeTime: 0
+          closeTime: 0,
         },
         {
           dayID: 4,
           dayOfWeek: "Friday",
           openTime: 0,
-          closeTime: 0
+          closeTime: 0,
         },
         {
           dayID: 5,
           dayOfWeek: "Saturday",
           openTime: 0,
-          closeTime: 0
+          closeTime: 0,
         },
-        
-      ]
+      ],
     };
   },
   methods: {
-    getRestaurantHours(restaurantId){
-      apiService.getBusinessByID(restaurantId)
-        .then(response => {
-          response.
-        })
-    }
+    getRestaurantDetails(restaurantId, index) {
+      if (this.displayDetails[index] === true) {
+        this.displayDetails[index] = false;
+      } else this.displayDetails[index] = true;
+      apiService.getBusinessByID(restaurantId).then((response) => {
+        this.rawDataDetails = response.data;
+      });
+    },
   },
-  created() {
-    //this.restaurantList = this.$store.returnedRestaurants
+  computed: {},
+  updated() {
+    console.log(this.$store.state.returnedRestaurants.businesses.length);
+    for (let i = 0; i < this.$store.state.returnedRestaurants.businesses.length; i++) {
+      this.displayDetails[i] = false;
+    }
   },
 };
 </script>
--- vue/src/components/RestaurantSearch.vue
+++ vue/src/components/RestaurantSearch.vue
@@ -31,7 +31,6 @@ export default {
       category: "",
       selectedCuisine: "",
       cuisines: [],
-      returnedRestaurants: [],
     };
   },
   created() {
